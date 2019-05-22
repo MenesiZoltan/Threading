7dfebbb86f785117885ffb4a09f127a46b230e8e
MenesiZoltan <colos.menesi@gmail.com>
2019-05-22 12:51:32 +0200

    implemented first 2 tasks

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex myVar = null;
+            const string mutexName = "RUNMEONLYONCE";
+            int counter = 0;
+
+            while (true)
+            {
+                try
+                {
+                    myVar = Mutex.OpenExisting(mutexName);
+                    myVar.Close();
+                    Console.WriteLine(counter);
+                    Console.ReadLine();
+                    counter++;
+                    Environment.Exit(1);
+                }
+                catch (WaitHandleCannotBeOpenedException e)
+                {
+                    Console.WriteLine($"Error caught, {mutexName} can not be closed. Iteration {counter}. \n{e}");
+                    Console.ReadLine();
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart threadStartDelegate = Counting;
+            Thread thread1 = new Thread(threadStartDelegate);
+            Thread thread2 = new Thread(threadStartDelegate);
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.ReadLine();
+        }
+
+        public static void Counting()
+        {
+            int counter;
+            for (int i = 1; i <= 10; i++)
+            {
+                counter = i;
+                Console.WriteLine($"Current count is:{i}.    Thread is: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
