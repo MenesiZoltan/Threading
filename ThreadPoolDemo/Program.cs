ae573ce718f6e935670b127bd060a0c4a49de09b
MenesiZoltan <colos.menesi@gmail.com>
2019-05-22 14:19:20 +0200

    implemented last task

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback delegateMethod = ShowMyText;
+            ThreadPool.QueueUserWorkItem(delegateMethod, "test1");
+            ThreadPool.QueueUserWorkItem(delegateMethod, "test2");
+            ThreadPool.QueueUserWorkItem(delegateMethod, "test3");
+            ThreadPool.QueueUserWorkItem(delegateMethod, "test4");
+            ThreadPool.QueueUserWorkItem(delegateMethod, "test5");
+
+            Console.ReadLine();
+        }
+
+        public static void ShowMyText(Object state)
+        {
+            Thread.Sleep(100);
+            string stateString = (string)state;
+            Console.WriteLine($"Tis is my text printer! Object to String is: {stateString}. The thread id is: {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
